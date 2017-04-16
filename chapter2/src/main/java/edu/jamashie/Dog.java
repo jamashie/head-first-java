e6b47cf696aa20ec5c7d6e2b32910f45a8828fdc
Jam Ashie <bampkins@gmail.com>
2017-04-16 14:15:04 -0500

    This is my first java class

---
+++ chapter2/src/main/java/edu/jamashie/Dog.java
@@ -0,0 +1,14 @@
+package edu.jamashie;
+
+/**
+ * Created by bampk on 4/16/2017.
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff! Ruff");
+    }
+}
---
+++ chapter2/src/main/java/edu/jamashie/DogTestDrive.java
@@ -0,0 +1,13 @@
+package edu.jamashie;
+
+/**
+ * Created by bampk on 4/16/2017.
+ */
+public class DogTestDrive {
+    public static void main (String[] args){
+        // Dog test code goes here
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
