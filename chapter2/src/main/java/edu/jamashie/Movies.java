282a14d0844cb52c41958335e7f41f8cfd4611ae
Jam Ashie <bampkins@gmail.com>
2017-04-18 20:05:29 -0500

    Movie test gto play

---
+++ chapter2/src/main/java/edu/jamashie/Movies.java
@@ -0,0 +1,14 @@
+package edu.jamashie;
+
+/**
+ * Created by bampk on 4/18/2017.
+ */
+public class Movies {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playing the movie");
+    }
+}
